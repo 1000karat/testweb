8aace932804c14ea1bb9a9c37c4b2eb721fd18c7
1000karat <92o3o92562@gmail.com>
2022-07-28 21:37:32 +0300

    Add chromedriver manager

--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,8 +16,9 @@ public class TestApp {
 
     @BeforeAll
     static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
         //System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
+        //System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
     }
 
     @BeforeEach
